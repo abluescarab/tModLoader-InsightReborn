eaa75602fffe86a3dd68d9334d4236d301605555
Alana Gilston <abluescarab@users.noreply.github.com>
2016-08-04 00:41:17 -0700

    Initial commit

---
+++ GlobalChestTile.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace InsightReborn {
+    public class GlobalChestTile : GlobalTile {
+        public override bool Autoload(ref string name) {
+            return true;
+        }
+
+        public override void MouseOver(int i, int j, int type) {
+            InsightReborn.ChestContents.Open = false;
+            base.MouseOver(i, j, type);
+        }
+
+        public override void MouseOverFar(int i, int j, int type) {
+            if(TileLoader.IsChest(type)) {
+                if(!InsightReborn.ChestContents.Open) {
+                    InsightReborn.ChestContents.SetItems(GetChest(i, j, type));
+                    InsightReborn.ChestContents.Open = true;
+                }
+            }
+            else {
+                InsightReborn.ChestContents.Open = false;
+            }
+
+            base.MouseOverFar(i, j, type);
+        }
+
+        private int GetChest(int i, int j, int type) {
+            int left = i;
+            int top = j;
+            Tile tile = Main.tile[i, j];
+
+            if(tile.frameX % 36 != 0) {
+                left--;
+            }
+            if(tile.frameY != 0) {
+                top--;
+            }
+
+            return Chest.FindChest(left, top);
+        }
+    }
+}
---
+++ InsightReborn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
+
+namespace InsightReborn {
+    public class InsightReborn : Mod {
+        public static ChestContentsUI ChestContents { get; set; }
+
+        public override void Load() {
+            Properties = new ModProperties() {
+                Autoload = true,
+                AutoloadBackgrounds = true,
+                AutoloadSounds = true
+            };
+            
+            ChestContents = new ChestContentsUI(this.Name, new Vector2(10, 35));
+            AddGlobalTile("GlobalChestTile", new GlobalChestTile());
+        }
+        
+        public override void PostDrawInterface(SpriteBatch spriteBatch) {
+            if(ChestContents.Open) {
+                ChestContents.Draw(spriteBatch);
+            }
+
+            base.PostDrawInterface(spriteBatch);
+        }
+    }
+}
