f2e7f5d037f390e3d713ff96bfe35086a616d94f
Alana Gilston <abluescarab@users.noreply.github.com>
2017-07-26 09:24:10 -0700

    v2.0.0

--- InsightReborn.cs
+++ InsightReborn.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using ModConfiguration;
 using Terraria.ModLoader;
 
 namespace InsightReborn {
     public class InsightReborn : Mod {
+        public const string REQUIRES_ACCESSORY = "RequiresAccessory";
+
         public static ChestContentsUI ChestContents { get; set; }
 
         public override void Load() {
@@ -12,11 +15,15 @@ namespace InsightReborn {
                 AutoloadBackgrounds = true,
                 AutoloadSounds = true
             };
-            
+
+            ModConfig.ModName = "InsightReborn";
+            ModConfig.AddOption(REQUIRES_ACCESSORY, true);
+            ModConfig.Load();
+
             ChestContents = new ChestContentsUI(this.Name, new Vector2(10, 35));
             AddGlobalTile("GlobalChestTile", new GlobalChestTile());
         }
-        
+
         public override void PostDrawInterface(SpriteBatch spriteBatch) {
             if(ChestContents.Open) {
                 ChestContents.Draw(spriteBatch);
---
+++ XRayGoggles.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace InsightReborn {
+    public class XRayGoggles : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("X-Ray Goggles");
+            Tooltip.SetDefault("Shows the contents of chests from far away");
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 0, 50, 0);
+            item.rare = 1;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
+            player.GetModPlayer<InsightRebornPlayer>().accXRayGoggles = true;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Lens, 2);
+            recipe.AddIngredient(ItemID.Ruby, 2);
+            recipe.AddTile(TileID.Chairs);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
